21921a691184230039d971d29cdfb39fd6a9ec30
nargok <nargok@gmail.com>
2021-06-26 08:43:07 +0900

    amend style

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import App from '@/App.vue';
+import router from '@/router';
+import store from '@/store';
+import VueCompositionAPI from '@vue/composition-api';
 
 Vue.config.productionTip = false;
 
@@ -10,3 +11,5 @@ new Vue({
   store,
   render: (h) => h(App),
 }).$mount('#app');
+
+Vue.use(VueCompositionAPI);
