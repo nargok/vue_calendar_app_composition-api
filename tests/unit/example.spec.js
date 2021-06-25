e5e716689cbfcaf67f575fbad1dd2a5af2b04cbc
nargok <nargok@gmail.com>
2021-06-26 08:45:53 +0900

    amend test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HomeComponent from '@/components/HOme.vue';
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+  it('renders text when passed', () => {
+    const text = 'ホーム';
+    const wrapper = shallowMount(HomeComponent);
+    expect(wrapper.text()).toMatch(text);
   });
 });
