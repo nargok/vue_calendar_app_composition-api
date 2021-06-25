ae5a7a36d582f8968479a766f68fba431415e3b2
nargok <nargok@gmail.com>
2021-06-26 08:51:23 +0900

    views templates

---
+++ src/views/Calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>カレンダー</h1>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  setup() {},
+});
+</script>
